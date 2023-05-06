45d83dfb3e27fbe72e6b994c18a5751c78c40716
jadeswanepoel <125471787+jadeswanepoel@users.noreply.github.com>
2023-05-06 23:23:04 +0200

    done

---
+++ js/index.js
@@ -0,0 +1,79 @@
+var firstName = "Jade"
+var surname = "Swanepoel"
+let age = 20
+const subject = "Interactive Development"
+
+let message = "Hello, my name is " + firstName + " " + surname + ", I am " + age + " years old and I am studying " + subject
+
+console.log(message)
+
+
+
+let one = 3
+let two = 0
+let three =10
+let four = 5
+
+let teamTotal = one + two
+let victor = three * four
+let total = teamTotal / victor
+
+let answer = "The answer is " + total
+
+console.log(answer)
+
+
+
+let Timmy = 10 
+let taxman1 = 15
+
+let group = Timmy / taxman1
+
+let answer1 = "Timmy owes the Taxman " + "R" + group
+
+console.log(answer1)
+
+
+
+
+let Susan = 25 
+let taxman2 = 26
+
+let group1 = Susan / taxman2
+
+let answer2 = "Susan owes the Taxman " + "R" + group1
+
+console.log(answer2)
+
+let answer4 = group + group1
+
+
+let answer3 = "In total, Timmy an Susan owes the Taxman " + "R" + answer4
+
+console.log(answer3)
+
+
+
+
+let John = 1
+let Tim = 1
+let Alfie = 1
+let Gwen = 1
+let constant = 4
+let test = 100
+
+let value = John / constant
+
+let result1 = "The student average is: " + value + "/100"
+console.log(result1)
+
+
+
+let newvalue = 0.07
+let value2 = value + newvalue
+
+let result2 = "The new student average is " + value2 + "/100"
+console.log(result2)
+
+
+
